178223d539bf9dbe4096fabc84c871b7bf7fb304
LTate12 <157613144+LTate12@users.noreply.github.com>
2026-01-29 10:37:35 -0500

    Tween type thing
    +Added SetTweenType method

--- Assets/CameraShake.cs
+++ Assets/CameraShake.cs
@@ -6,6 +6,7 @@ public class CameraShake : MonoBehaviour
     public bool shakeStart = false;
     public float duration = 1f;
     public float shakeIntensity = 1f;
+    private float enumElapsedTime = 0f;
 
     public AnimationCurve shakeCurve;
     
@@ -32,6 +33,7 @@ public class CameraShake : MonoBehaviour
         while (elapsedTime < duration)
         {
             elapsedTime += Time.deltaTime;
+            enumElapsedTime = elapsedTime;
             float strength = shakeCurve.Evaluate(elapsedTime/duration);
             transform.position = startPosition + (Random.insideUnitSphere * strength)* shakeIntensity;
             yield return null;
@@ -57,5 +59,27 @@ public class CameraShake : MonoBehaviour
         duration = _duration;
         shakeIntensity = _intensity;
         shakeStart=true;
+    }
+
+    /* setTweenType - Sets the tween type of the camera shake curve
+     * @param _tweenType - The type of tween to use for the camera shake curve. Can choose between Linear, Ease In, and Constant.
+     * @param optionalvalue - Optional value parameter, use if you want a constant curve type that uses a different shake curve key. Default key: 1.
+     */
+    public void setTweenType(string _tweenType, int optionalvalue = 1)
+    {
+        if (_tweenType.Equals("EaseIn") || _tweenType.Equals("Ease In") || _tweenType.Equals("ease in") || _tweenType.Equals("easein"))
+        {
+            shakeCurve = AnimationCurve.EaseInOut(enumElapsedTime, shakeCurve.keys[1].value, (enumElapsedTime * duration) + 1, shakeCurve.keys[2].value);
+        }
+
+        if (_tweenType.Equals("Linear") || _tweenType.Equals("linear"))
+        {
+            shakeCurve = AnimationCurve.Linear(enumElapsedTime, shakeCurve.keys[1].value, (enumElapsedTime * duration) + 1, shakeCurve.keys[2].value);
+        }
+
+        if (_tweenType.Equals("Constant") || _tweenType.Equals("constant"))
+        {
+            shakeCurve = AnimationCurve.Constant(enumElapsedTime, (enumElapsedTime * duration) + 1, shakeCurve.keys[optionalvalue].value);
+        }
     }
 }
